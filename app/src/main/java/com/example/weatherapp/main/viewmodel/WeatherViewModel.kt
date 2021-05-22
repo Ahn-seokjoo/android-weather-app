29f74ad887180b4e5a80bd789e4b51f7316cf9c0
ahn seokjoo <tjrwn0716@naver.com>
2021-05-23 03:42:33 +0900

    불필요 구조 삭제

--- app/src/main/java/com/example/weatherapp/main/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/main/viewmodel/WeatherViewModel.kt
@@ -1,23 +1,8 @@
 package com.example.weatherapp.main.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.weatherapp.repository.MemoryWeatherRepository
 import com.example.weatherapp.repository.WeatherRepository
-import com.example.weatherapp.repository.WeatherResult
 
 class WeatherViewModel : ViewModel() {
-    private val repository = MemoryWeatherRepository()
     val weatherRepo = WeatherRepository()
-
-    private val _weatherListLiveData = MutableLiveData<List<List<WeatherResult.WeathersResponse>>>()
-
-    val weatherListLiveData: LiveData<List<List<WeatherResult.WeathersResponse>>>
-        get() = _weatherListLiveData
-
-    fun updateWeatherList(weather: List<WeatherResult.WeathersResponse>) {
-        repository.updateWeatherList(weather)
-        _weatherListLiveData.value = repository.weatherList
-    }
 }
